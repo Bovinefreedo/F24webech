9eb7459cab6e062167e61c2026bdb9a7a41dbc0a
Frank Skorstengaard <1612-au651342@user.noreply.gitlab.au.dk>
2024-08-07 20:39:56 +0200

    Co-authored-by: HilinaHilu <HilinaHilu@users.noreply.github.com>

--- objecter.js
+++ objecter.js
@@ -6,7 +6,7 @@ let input = {
         ["Munich", "Germany"],
         ["Berlin", "Germany"],
         ["New York", "USA"],
-        ["Los Angeles, USA"]
+        ["Los Angeles", "USA"]
 
     ],
     dogs: [{name:"Bob", age:1, breed:"old english sheepdog"},
@@ -17,12 +17,93 @@ let input = {
     ]
 }
 
+let input2 = {
+    status: 404
+}
 
-//Skriv en function som siger den er valid hvis status er 200
-isValid(input)
+//console.log(input2.status)
+
+//Du skal skrive "poodle" i consolen
+//console.log(input.dogs[3].breed)
+
+//Skriv "Odense" i konsolen
+//console.log(input.countryData[1][0])
+
+//Skriv en function som siger den er valid hvis status er 200, det betyder at den skal retunere true, hvis ikke der står 200 skal der stå false. 
+
+//console.log(isValid(input2))
+
+function isValid(incomingData){
+    if(200===incomingData.status){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+//Lav en function der retunere alle landene
+function printCountries(input){
+    if(isValid(input)){
+        let countries = [];
+        input.countryData.forEach(data => {
+            countries.push(data[1]);
+        });
+        return countries;
+    }
+    else{return "no valid input"}
+}
+
+//console.log(printCountries(input));
+//console.log(printCountries(input2));
+
 
 //Skriv en funktion som retunere navne på de byer der i det andet argument.
-citiesInCountry(input, "Denmark")
+
+
+function citiesInCountry(input, country){
+    let cities=[]; 
+    input.countryData.forEach(data=>{
+        if(country===data[1]){
+            cities.push(data[0]);            
+        }
+    });
+    return cities;
+}
+
+//console.log(citiesInCountry(input, "Denmark"))
+//citiesInCountry(input, "England")
 
 //Skriv en funktion som udskriver alle nave, og aldre på hundene i vores input.
+
+
+function dogsNamesAge(input){
+    for(let i=0; i<input.dogs.length; i++){
+        console.log(input.dogs[i].name, input.dogs[i].age, input.dogs[i].breed);
+    }
+}
+
 dogsNamesAge(input)
+
+///////////////////////////////////////////////////////////
+//Større opgave////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+
+//Lav en function der kan lave katte objekter, 
+//Katten skal have 3 atributes 
+//en kat har et navn, en alder og en farve
+//Vores katte skal have 3 metoder disse metoder skal kunne method chaine (der skal kunne stå meow og purr purr, med kun en henvisning til objectet)
+//meow, der skriver "meow" i consolen
+//attack, der skriver "scratch scrach" i konsolen
+//purr, der skriver "purr purr" i consolen".
+
+
+//Bagefter skal du kunne instantiate en kat ved kald af funktionen
+//Få skrevet, navn, alder og farve i consolen
+//Du skal kunne lave en method chain ud fra din kat
+
+
+
+///////////////////////////////////
+//HUSK ALTID {} EFTER IF(){}, for(){}, fuction name(){}
